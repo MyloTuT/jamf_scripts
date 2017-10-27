53b176143c090cd065845bfd95bc7ac043d143fb
Myles Rayfield <mylesrayfield@C02TN9V8HTD6.local>
2017-10-27 10:37:21 -0400

    ssh

--- policy/enable_ssh.sh
+++ policy/enable_ssh.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-# Enable ARD & VNC
-/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -allUsers -privs -all -clientopts -setvnclegacy -vnclegacy yes -setvncpw -vncpw ******** -restart -agent
+#!/bin/bash 
 
-# Enable SSH
-systemsetup -setremotelogin on
+# Enable ARD, Remote Management, and Remote Login (SSH) \- 1. Removes Administrators Group from Remote login, 2 & 3. Creates xxxxxxxxx Membership, 4 & 5. Adds xxxxxxxxx User to Remotelogin then activates.
+
+sudo dseditgroup -o edit -d admin -t group com.apple.access_ssh
+sudo dscl . append /Groups/com.apple.access_ssh user jamfadmin
+sudo dscl . append /Groups/com.apple.access_ssh GroupMembership Administrators
+sudo dscl . append /Groups/com.apple.access_ssh groupmembers `dscl . read /Users/xxxxxxxxx GeneratedUID | cut -d " " -f 2`
+sudo systemsetup -setremotelogin on
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -specifiedUsers
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -users jamfadmin -access -on -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -activate -restart -console
